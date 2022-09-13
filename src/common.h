13ef3a8687f0e0993ec6080b33c89e1f9693432c
Shreerang Vaidya <smv@pop-os.localdomain>
2022-09-13 17:07:50 +0530

    refactored a bit for transpiling

--- src/common.h
+++ src/common.h
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define loop for(;;)
+#define loop for (;;)
 #define NO_CHAR (-1)
 // not _that_ common but oh well
 #define REPL_MAX_CHARS (1024)
@@ -17,5 +17,15 @@ typedef uint16_t u16;
 typedef uint32_t u32;
 typedef int32_t i32;
 // yes I _know_ that I'm typedefing a typedef typedef
+
+static const char *help_message =
+    "Help:\n"
+    "-h           - print this message\n"
+    "-c <file>    - compile to C code (out.c)\n"
+    "-f <file>    - fun a file\n"
+    "<no options> - run REPL mode\n";
+
+char *read_file(char *file_name);
+void write_file(char *file_name);
 
 #endif
---
+++ src/transpiler.c
@@ -0,0 +1,7 @@
+#include "transpiler.h"
+
+#include "common.h"
+
+void Compile(char *file_name) {
+  
+}
